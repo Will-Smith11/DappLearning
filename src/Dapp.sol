2b4f02b1983a5f7d8e28e099e6122159abffc61d
Will Smith <williamqsmith11@gmail.com>
2021-12-31 08:50:08 -0500

    base commit

--- src/Dapp.sol
+++ src/Dapp.sol
@@ -1,5 +1,28 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-contract Dapp {
+
+contract Dapp
+{
+    string ContractPassword;
+    address owner;
+    
+    constructor()
+    {
+        owner = msg.sender;
+    }
+    
+    modifier isOwner()
+    {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function getBalance() external view returns (uint)
+    {
+        return address(this).balance;
+    }
+
+
+
 }
